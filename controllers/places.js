a03469aa0f0101e9ec2f0c140758d57070a38d82
Roman M <rmarchen95@gmail.com>
2023-02-13 23:00:10 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 // NEW /places
 router.get('/new', (req, res) => {
@@ -9,25 +10,37 @@ router.get('/new', (req, res) => {
 // POST
 router.post('/', (req, res) => {
     console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+        // Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
+
 
 
 // GET /places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'Office Coffee and Cocktails',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Coffee/ Cocktails',
-        pic: '/images/coffee-shop.jpg'
-    }, {
-        name: 'Tokyo Ramen',
-        city: 'New York',
-        state: 'NY',
-        cuisines: 'Asian Cuisine',
-        pic: '/images/tokyo-ramen.jpg'
-    }]
+    // let places = [{
+    //     name: 'Office Coffee and Cocktails',
+    //     city: 'Seattle',
+    //     state: 'WA',
+    //     cuisines: 'Coffee/ Cocktails',
+    //     pic: '/images/coffee-shop.jpg'
+    // }, {
+    //     name: 'Tokyo Ramen',
+    //     city: 'New York',
+    //     state: 'NY',
+    //     cuisines: 'Asian Cuisine',
+    //     pic: '/images/tokyo-ramen.jpg'
+    // }]
 
     res.render('places/index', { places })
 })
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Office Coffee and Cocktails',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Coffee/ Cocktails',
+    pic: '/images/coffee-shop.jpg'
+}, {
+    name: 'Tokyo Ramen',
+    city: 'New York',
+    state: 'NY',
+    cuisines: 'Asian Cuisine',
+    pic: '/images/tokyo-ramen.jpg'
+}]
