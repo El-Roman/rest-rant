002540f4ea4e044671440b2c5b12d6db9a334089
Roman M <rmarchen95@gmail.com>
2023-02-15 01:01:22 -0800

    debugging last step for adding edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,10 +1,29 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
+// // index
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
 
 // NEW /places
 router.get('/new', (req, res) => {
     res.render('places/new')
+})
+
+// EDIT
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', { place: places[id] })
+    }
 })
 
 
@@ -22,19 +41,6 @@ router.get('/:id', (req, res) => {
     }
 })
 
-// EDIT
-router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-        res.render('places/edit', { place: places[id] })
-    }
-})
 
 // POST
 router.post('/', (req, res) => {
@@ -53,13 +59,6 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
 
-
-
-// // GET /places
-router.get('/', (req, res) => {
-    res.render('places/index', { places })
-})
-
 // DELETE
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
@@ -75,6 +74,33 @@ router.delete('/:id', (req, res) => {
     }
 })
 
+// PUT
+router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places`)
+    }
+})
 
 
 
