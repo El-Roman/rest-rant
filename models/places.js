a41e280aee63d13f7d1c5f9df03c8fc2c1871045
Roman M <rmarchen95@gmail.com>
2023-02-19 22:46:09 -0800

    mongoose
    Added mongoose DB integration

--- models/places.js
+++ models/places.js
@@ -1,13 +1,12 @@
-module.exports = [{
-    name: 'Office Coffee and Cocktails',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Coffee/ Cocktails',
-    pic: '/images/coffee-shop.jpg'
-}, {
-    name: 'Tokyo Ramen',
-    city: 'New York',
-    state: 'NY',
-    cuisines: 'Asian Cuisine',
-    pic: '/images/tokyo-ramen.jpg'
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: String,
+    cuisines: { type: String, required: true },
+    city: { type: String, default: 'Anytown' },
+    state: { type: String, default: 'USA' },
+    founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
